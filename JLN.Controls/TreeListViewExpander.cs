aa5da37fcab331da23c5160c55bb61512feaba36
Johan Larsson <johan_gavle1@hotmail.com>
2013-11-05 01:37:14 +0100

    Initial

---
+++ JLN.Controls/TreeListViewExpander.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls.Primitives;
+
+namespace JLN.Controls
+{
+    /// <summary>
+    /// Represents a control that can switch states in order to expand a node of a TreeListView.
+    /// </summary>
+    public class TreeListViewExpander : ToggleButton { }
+}
