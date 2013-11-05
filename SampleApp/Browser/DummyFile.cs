5afe019b6a85d9f83b31851a7f679ae4e73631d2
Johan Larsson <johan_gavle1@hotmail.com>
2013-11-05 20:05:52 +0100

    ScrollSynchronizer

---
+++ SampleApp/Browser/DummyFile.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace SampleApp.Browser
+{
+    public class DummyFile
+    {
+        public DummyFile(FileInfo fileInfo)
+        {
+            FileInfo = fileInfo;
+        }
+
+        public FileInfo FileInfo { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return FileInfo.Name;
+            }
+        }
+
+        public string Type { get { return "File"; } }
+    }
+}
