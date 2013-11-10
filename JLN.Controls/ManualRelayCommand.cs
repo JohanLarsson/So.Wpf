0c8ad2aaade3909a6c6629cc61dfc809c8ceba27
Johan Larsson <johan_gavle1@hotmail.com>
2013-11-10 11:06:38 +0100

    ManualRelayCommand

---
+++ JLN.Controls/ManualRelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JLN.Controls
+{
+    public class ManualRelayCommand : RelayCommandBase
+    {
+        public ManualRelayCommand(Action<object> action, Predicate<object> condition) : base(action, condition)
+        {
+        }
+
+        public ManualRelayCommand(Action<object> action) : base(action)
+        {
+        }
+
+        
+        public override event EventHandler CanExecuteChanged;
+
+        public virtual void RaiseExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null) 
+                handler(this, EventArgs.Empty);
+        }
+    }
+}
--- JLN.Controls/RelayCommand.cs
+++ JLN.Controls/RelayCommand.cs
@@ -3,36 +3,20 @@ using System.Windows.Input;
 
 namespace JLN.Controls
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand : RelayCommandBase
     {
-        private readonly Action<object> _action;
-        private readonly Predicate<object> _condition;
-        public RelayCommand(Action<object> action, Predicate<object> condition)
+        public RelayCommand(Action<object> action, Predicate<object> condition) : base(action, condition)
         {
-            _action = action;
-            _condition = condition ?? (o => true);
         }
 
-        public RelayCommand(Action<object> action)
+        public RelayCommand(Action<object> action) : base(action)
         {
-            _action = action;
-            _condition = (o) => true;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return _condition(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            _action(parameter);
         }
 
         /// <summary>
         /// http://stackoverflow.com/a/2588145/1069200
         /// </summary>
-        public event EventHandler CanExecuteChanged
+        public override event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
