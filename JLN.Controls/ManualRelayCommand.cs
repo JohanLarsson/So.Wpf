4378cda6936d328845b60a7547295bdb6446c8fb
Johan Larsson <johan_gavle1@hotmail.com>
2013-12-01 08:56:05 +0100

    Fixed ManualRelayComand RaiseCanExecuteChanged

--- JLN.Controls/ManualRelayCommand.cs
+++ JLN.Controls/ManualRelayCommand.cs
@@ -1,25 +1,30 @@
 ﻿using System;
+using System.Windows;
 
 namespace JLN.Controls
 {
     public class ManualRelayCommand : RelayCommandBase
     {
-        public ManualRelayCommand(Action<object> action, Predicate<object> condition) : base(action, condition)
+        public ManualRelayCommand(Action<object> action, Predicate<object> condition)
+            : base(action, condition)
         {
         }
 
-        public ManualRelayCommand(Action<object> action) : base(action)
+        public ManualRelayCommand(Action<object> action)
+            : base(action)
         {
         }
 
-        
+
         public override event EventHandler CanExecuteChanged;
 
         public virtual void RaiseCanExecuteChanged()
         {
             var handler = CanExecuteChanged;
-            if (handler != null) 
-                handler(this, EventArgs.Empty);
+            if (handler != null)
+            {
+                Application.Current.Dispatcher.InvokeAsync(() => handler(this, EventArgs.Empty));
+            }
         }
     }
 }
